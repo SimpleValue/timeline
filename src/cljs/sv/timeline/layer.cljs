62b183e235211b2815f37e756104b130f04f169c
nils1689 <nils@vire.co>
2020-09-30 17:15:45 +0200

    set layer height

--- src/cljs/sv/timeline/layer.cljs
+++ src/cljs/sv/timeline/layer.cljs
@@ -5,5 +5,5 @@
   [layer timeline-state]
   [:div {:style {:background-color "#eeeeee"
                  :width "100%"
-                 :height "100px"}}
+                 :height (:height layer)}}
    [element/component layer timeline-state]])
